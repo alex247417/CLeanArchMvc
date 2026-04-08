0bed789c3855193b5344cbbe6a0b6f54cfe14d04
Alecsandro <alex247417@hotmail.com>
2026-04-08 09:23:40 -0300

    chore: initial project setup with clean architecture structure

---
+++ CLeanArchMvc.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CLeanArchMvc.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
